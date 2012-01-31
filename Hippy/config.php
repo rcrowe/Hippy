abe06fe2f364ecc359e95d671e3412662a97eeab
Rob Crowe <hello@vivalacrowe.com>
2012-01-31 00:14:26 +0000

    Put back dummy config

--- Hippy/config.php
+++ Hippy/config.php
@@ -11,12 +11,12 @@ return array(
 	 * API token to access the Hipchat with. This can be found at
 	 * https://#account#.hipchat.com/admmin/api
 	 */
-	'token'  => 'd5787f90331e952c2ed4dc6811b2ae',
+	'token'  => 'abc123',
 	
 	/**
 	 * Name of the room you want the message sent too
 	 */
-	'room'   => 'Tiny Rocketship',
+	'room'   => 'Hippy Test',
 	
 	/**
 	 * Name of the user who the message will appear to be from
@@ -43,5 +43,5 @@ return array(
 	 * URL to the Hipchat API endpoint. Shouldn't need to change this, mainly for testing purposes.
 	 * If you see errors about SSL certificate, you should probably fix it, or change https:// to http://
 	 */
-	'api_endpoint' => 'http://api.hipchat.com/v1/',
+	'api_endpoint' => 'https://api.hipchat.com/v1/',
 );
