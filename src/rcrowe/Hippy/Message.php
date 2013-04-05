78bc28a09f35bbd9739aba447626cc73f4249717
Rob Crowe <hello@vivalacrowe.com>
2013-04-05 22:54:01 +0100

    More docblocks

--- src/rcrowe/Hippy/Message.php
+++ src/rcrowe/Hippy/Message.php
@@ -1,19 +1,49 @@
 <?php
 
+/**
+ * PHP client for HipChat. Designed for incidental notifications from an application.
+ *
+ * @author Rob Crowe <hello@vivalacrowe.com>
+ * @copyright Copyright (c) 2013, Rob Crowe.
+ * @license MIT
+ */
+
 namespace rcrowe\Hippy;
 
 use rcrowe\Hippy\Message\SenderInterface;
 use rcrowe\Hippy\Message\MessageInterface;
 
+/**
+ * Holds the message and meta data about the message.
+ */
 class Message implements SenderInterface, MessageInterface
 {
+    /**
+     * @var bool Does this message generate a notification. Default false.
+     */
     protected $notification;
+
+    /**
+     * @var string Background color of the message. Default green.
+     */
     protected $background_color;
+
+    /**
+     * @var string Actual message.
+     */
     protected $message;
+
+    /**
+     * @var string Is the message plain text or html.
+     */
     protected $message_format;
 
+    /**
+     * {@inheritdoc}
+     */
     public function __construct($notify = false, $background_color = 'yellow')
     {
+        // Use constant for the color
         $this->notification     = $notify;
         $this->background_color = $background_color;
     }
@@ -26,27 +56,42 @@ class Message implements SenderInterface, MessageInterface
         return $this->notification;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setNotification()
     {
         $this->notification = true;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function removeNotification()
     {
         $this->notification = false;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getBackgroundColor()
     {
         return $this->background_color;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setBackgroundColor($color)
     {
         // throw exception here if not a valid color
         $this->background_color = $color;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setHtml($html)
     {
         // According to the docs I need to encode
@@ -57,11 +102,19 @@ class Message implements SenderInterface, MessageInterface
         $this->setMessage($html, static::FORMAT_HTML);
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setText($text)
     {
         $this->setMessage($text, static::FORMAT_TEXT);
     }
 
+    /**
+     * @param string $text
+     * @param string format
+     * @return void
+     */
     protected function setMessage($text, $format)
     {
         // throw exception if longer than 10,000 chars
@@ -69,11 +122,17 @@ class Message implements SenderInterface, MessageInterface
         $this->message_format = $format;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getMessage()
     {
         return $this->message;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getMessageFormat()
     {
         return $this->message_format;
--- src/rcrowe/Hippy/Message/MessageInterface.php
+++ src/rcrowe/Hippy/Message/MessageInterface.php
@@ -1,20 +1,94 @@
 <?php
 
+/**
+ * PHP client for HipChat. Designed for incidental notifications from an application.
+ *
+ * @author Rob Crowe <hello@vivalacrowe.com>
+ * @copyright Copyright (c) 2013, Rob Crowe.
+ * @license MIT
+ */
+
 namespace rcrowe\Hippy\Message;
 
+/**
+ * All messages sent or added to a queue must implement this.
+ */
 interface MessageInterface
 {
     const FORMAT_TEXT = 'text';
     const FORMAT_HTML = 'html';
 
+    /**
+     * Get a new instance of message.
+     *
+     * @param bool   $notify           Should this message create a notification.
+     * @param string $background_color Background color of message.
+     */
     public function __construct($notify = false, $background_color = 'yellow');
+
+    /**
+     * Will this message generate a notification.
+     *
+     * @return bool
+     */
     public function getNotification();
+
+    /**
+     * Set that the message will generate a notification.
+     *
+     * @return void
+     */
     public function setNotification();
+
+    /**
+     * Set that the message will not generate a notification.
+     *
+     * @return void
+     */
     public function removeNotification();
+
+    /**
+     * Get the background color.
+     *
+     * @return string
+     */
     public function getBackgroundColor();
+
+    /**
+     * Set the background color
+     *
+     * @param string $color.
+     * @return void
+     */
     public function setBackgroundColor($color);
+
+    /**
+     * Set the message as HTML.
+     *
+     * @param string $html
+     * @return void
+     */
     public function setHtml($html);
+
+    /**
+     * Set the message as plain text.
+     *
+     * @param string $text
+     * @return void
+     */
     public function setText($text);
+
+    /**
+     * Get the message.
+     *
+     * @return string
+     */
     public function getMessage();
+
+    /**
+     * Get which format the message is in.
+     *
+     * @return string
+     */
     public function getMessageFormat();
 }
