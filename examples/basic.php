a74a07cf2bb8591423f9e67896d9a564c744e879
Rob Crowe <hello@vivalacrowe.com>
2013-04-05 10:13:29 +0100

    Do not store composer lock file

--- examples/basic.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Include the Hippy library
-// Make sure Hippy/config.php is set with your Hipchat details
-require '../Hippy.php';
-
-// Send a message
-Hippy::speak('[Hippy Basic Example] - Hello world');
